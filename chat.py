1473c78f88956c5d5cc50909e07dd8821b06560f
Orange <orange@Orangede-MacBook-Pro.local>
2020-05-10 00:34:03 +0800

    convert and ouput

--- chat.py
+++ chat.py
@@ -1,4 +1,3 @@
-
 def read_file(input_file):
 	lines = []
 	with open(input_file, 'r', encoding = 'utf-8-sig') as f:
@@ -6,6 +5,27 @@ def read_file(input_file):
 			lines.append(line.strip())
 		return lines
 
-lines = read_file('input.txt')
-print(lines)
+def convert(lines, person_a, person_b):
+	new = []
+	person = None
+	for line in lines:
+		if line == person_a:
+			person = person_a
+			continue
+		elif line == person_b:
+			person = person_b
+			continue
+		new.append(person + ': ' + line)
+	return new
 
+def write_file(output_file, lines):
+	with open(output_file, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines, 'Allen', 'Tom')
+	write_file('ouput.txt', lines)
+
+main()
